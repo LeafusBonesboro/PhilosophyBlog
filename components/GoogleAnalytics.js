f8a0235094cb67d0813b6d8c590fe2a75f9be82f
LeafusBonesboro <108142251+LeafusBonesboro@users.noreply.github.com>
2025-09-17 10:49:06 -0400

    moreGoogleBAgain

--- components/GoogleAnalytics.js
+++ components/GoogleAnalytics.js
@@ -6,17 +6,18 @@ export default function GoogleAnalytics() {
   return (
     <>
       <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-MNQTVCEF9M"
-        strategy="afterInteractive"
-      />
-      <Script id="ga-init" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-MNQTVCEF9M');
-        `}
-      </Script>
+  src="https://www.googletagmanager.com/gtag/js?id=G-RVVXF2XV75"
+  strategy="afterInteractive"
+/>
+<Script id="ga-script" strategy="afterInteractive">
+  {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-RVVXF2XV75');
+  `}
+</Script>
+
     </>
   );
 }
